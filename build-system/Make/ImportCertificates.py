3858af3132600530094fca38852c199ccca59c17
MPAK1337 <67393332+MPAK1337@users.noreply.github.com>
2026-02-04 13:00:36 +0300

    Fix keychain search list setup for fake codesigning

--- build-system/Make/ImportCertificates.py
+++ build-system/Make/ImportCertificates.py
@@ -29,17 +29,18 @@ def import_certificates(certificatesPath):
         keychain_name
     ], check_result=True)
 
-    existing_keychains = run_executable_with_output('security', arguments=['list-keychains', '-d', 'user'])
-    existing_keychains.replace('"', '')
+    existing_keychains_output = run_executable_with_output('security', arguments=['list-keychains', '-d', 'user'])
+    existing_keychains = [
+        line.strip().strip('"')
+        for line in existing_keychains_output.splitlines()
+        if line.strip()
+    ]
 
-    run_executable_with_output('security', arguments=[
-        'list-keychains',
-        '-d',
-        'user',
-        '-s',
-        keychain_name,
-        existing_keychains
-    ], check_result=True)
+    run_executable_with_output(
+        'security',
+        arguments=['list-keychains', '-d', 'user', '-s', keychain_name, *existing_keychains],
+        check_result=True
+    )
 
     run_executable_with_output('security', arguments=['set-keychain-settings', keychain_name])
     run_executable_with_output('security', arguments=['unlock-keychain', '-p', keychain_password, keychain_name])
